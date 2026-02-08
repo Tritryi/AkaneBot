3927f83b2149202b7398607a05b89f5cb7f0179e
Tritryi <tristan.fontaniere@outlook.fr>
2026-02-08 03:08:40 +0100

    Passage au mode commands.bot pour plus de clareté

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import discord
+from discord.ext import commands
 import os
 from dotenv import load_dotenv
 load_dotenv(dotenv_path=".config")
@@ -9,40 +10,61 @@ intents = discord.Intents.default()
 # Ici pour qu'il lise les messages des utilisateurs (répondre à des pattern, des commandes)
 intents.message_content = True
 
-client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix="a!",intents=intents)
 
-@client.event
+@bot.event
 async def on_ready():
+    status = discord.CustomActivity(name="I'm on your side, no matter what happens")
+    await bot.change_presence(status=discord.Status.online,activity=status)
     print("Bot prêt !")
 
-@client.event
+
+@bot.event
 async def on_message(message):
     # On empêche le bot de répondre à lui-même
-    if message.author == client.user:
+    if message.author == bot.user:
         return
     # Petit ajout drôle d'exemple
     salutations = ["salut","bonjour","hello"]
     if any(mot in message.content.lower() for mot in salutations):
         await message.channel.send(f"Hello {message.author.name} !")
 
-    # Commande d'affichage de la doc
-    if message.content == "a!help":
-        with open ("help.md") as f:
-            contenu = (f.read())
-        await message.channel.send(contenu)
+    if "akane" in message.content.lower():
+        img = discord.File("./akane.webp")
+        await message.channel.send("On parle de moi ? La best girl", file=img)
+
+    await bot.process_commands(message)
+
+bot.remove_command("help")
+@bot.command(name="help")
+async def help(ctx):
+    with open("help.md") as f:
+        contenu = f.read()
     
+    embed = discord.Embed(
+        title="📖 Guide d'utilisation d'Akane <:akane_think:1469869490249531564>",
+        description=contenu,
+        color=discord.Color.blue()
+    )
+    await ctx.send(embed=embed)
+
+@bot.command()
+async def clear(ctx,number: int):
+    await ctx.channel.purge(limit=number+1)
+    await ctx.send(f"✅ J'ai supprimé {number} messages", delete_after=3)
+
     
-    # COMMANDES PLUS GENERALES
-    if message.content.startswith("a!del"):
-        # coupe "a!del 10" en deux et récupère le 2eme élément
-        number = message.content.split()
-        if len(number) < 2:
-            await message.channel.send("⚠️ Il manque le nombre de messages")
-            return
-        number = int(number[1])
-        # regarde l'historique de message, +1 pour la commande en elle-même
-        deleted = await message.channel.purge(limit=number+1)
-        await message.channel.send(f"✅ J'ai supprimé {number} messages", delete_after=3)
+#     # COMMANDES PLUS GENERALES
+#     if message.content.startswith("a!del"):
+#         # coupe "a!del 10" en deux et récupère le 2eme élément
+#         number = message.content.split()
+#         if len(number) < 2:
+#             await message.channel.send("⚠️ Il manque le nombre de messages")
+#             return
+#         number = int(number[1])
+#         # regarde l'historique de message, +1 pour la commande en elle-même
+#         deleted = await message.channel.purge(limit=number+1)
+#         await message.channel.send(f"✅ J'ai supprimé {number} messages", delete_after=3)
         
 
 
@@ -55,4 +77,4 @@ async def on_message(message):
 
 
 
-client.run(os.getenv("DISCORD_TOKEN"))
+bot.run(os.getenv("DISCORD_TOKEN"))
