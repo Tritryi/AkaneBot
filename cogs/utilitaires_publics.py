4e954efe2c7547511686ed5a1f83c2344c89bd43
Tritryi <tristan.fontaniere@outlook.fr>
2026-02-11 01:41:41 +0100

    Commentaires pour les utilitaires publics

--- cogs/utilitaires_publics.py
+++ cogs/utilitaires_publics.py
@@ -15,7 +15,9 @@ class UtilitairesPublics(commands.Cog):
         Arguments : 
             aucun
         """
+        # On récupère le contenu du help
         helpcontent = self.bot.helpcontent
+        # Création du embed
         embed = discord.Embed(
             title="📖 Guide d'utilisation d'Akane <:akane_think:1469869490249531564>",
             description=helpcontent,
@@ -23,6 +25,43 @@ class UtilitairesPublics(commands.Cog):
         )
         await ctx.send("Je t'ai envoyé l'aide en mp !", delete_after=2)
         await ctx.author.send(embed=embed)
+
+    @commands.command()
+    async def pres(self,ctx):
+        """
+        Envoie d'un bloc servant de présentation en utilisaent un embed et un dictionnaire pour les informations.
+
+        Arguments : 
+            aucun
+        """
+        # Ici on définit les informations qu'on veut inclure
+        datas = {
+            "Prénom" : "Akane",
+            "Nom" : "Kurokawa",
+            "Inspiration": "Je viens d'un animé qui s'appelle Oshi no Ko",
+            "Présentation": """En temps normal, je suis une jeune comédienne de 19 ans qui profite de sa jeunesse et a beaucoup de succès au théâtre tout comme au cinéma. Mais ici, je suis plutôt là pour vous aider à gérer cet endroit. J'aime quand les choses sont en ordre et j'aime beaucoup rigoler pour décompresser. Du coup je peux vous aider à modérer cet endroit !"""
+        }
+        # On créé une liste contenant nos deux images (thumbnail et bannière), on utilise discord.File car cela est obligé
+        # par discord.
+        images = ["logo.webp","illustration.webp"]
+        files_to_send = []
+        for x in images:
+            y = discord.File("./img/"+x, filename=x)
+            files_to_send.append(y)
+
+        # Création du embed 
+        embed = discord.Embed(
+            title="🤔 Qui est Akane ?",
+            color=discord.Color.dark_blue()
+        )
+        # Ajout du thumbnail et de l'image
+        embed.set_thumbnail(url="attachment://logo.webp")
+        embed.set_image(url="attachment://illustration.webp")
+        # Boucle permettant d'ajouter toutes nos informations
+        for new_data in datas:
+            embed.add_field(name=new_data,value=datas[new_data],inline=False)
+
+        await ctx.send(embed=embed,files=files_to_send)
 
     
     
