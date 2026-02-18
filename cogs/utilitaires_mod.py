9c60b21203a362a243a63c96e1863d95dc1ab09a
Tritryi <tristan.fontaniere@outlook.fr>
2026-02-18 02:00:24 +0100

    Ajout d'un event listener sur l'ajout de réaction sur notre message de rôles de base, le listener est fonctionnel et commenté

--- cogs/utilitaires_mod.py
+++ cogs/utilitaires_mod.py
@@ -98,6 +98,44 @@ class UtilitairesMod(commands.Cog):
         except TypeError as e:
             print(f"Emoji invalide : {e}")
 
+    @commands.Cog.listener()
+    async def on_raw_reaction_add(self,payload):
+        # chargement des variables
+        config = self.get_config()
+        msg_id = config["role_message_id"]
+        ch_id = config["role_channel_id"]
+        emoji_id = config["emoji_role_id"]
+        id_r1 = config["id_role1"]
+        id_r2 = config["id_role2"]
+
+        # Si la réaction concerne bien le message de setup des roles et que c'est le bon emoji ajouté, alors 
+        # on récupère les rôles que l'on veut ajouter
+        if payload.channel_id == ch_id and \
+           payload.message_id == msg_id and \
+           payload.emoji.id == emoji_id:
+            guild = payload.member.guild
+            role1 = guild.get_role(id_r1)
+            role2 = guild.get_role(id_r2)
+
+            # si les rôles ont bien été récupérés on les ajoute à l'utilisateur
+            if role1 and role2:
+                try:
+                    # en théorie on peut mettre une raison mais ça n'a pas vraiment d'intérêt
+                    await payload.member.add_roles(role1,role2)
+                except discord.Forbidden:
+                    print("Permissions insuffisantes pour ajouter ces rôles")
+                except discord.HTTPException:
+                    print("Échec de l'ajout des rôles")
+            else:
+                print("Un des rôles n'a pas pu être récupéré")
+                return
+            
+        # dans le cas où une réaction concerne un autre message ou un autre emoji, on ignore   
+        else:
+            return
+
+
+
 
 
 
