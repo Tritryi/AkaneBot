e4b212a9b55a0644fae36969599f30ad19c230a6
Tritryi <tristan.fontaniere@outlook.fr>
2026-03-01 00:29:59 +0100

    Commentaire de la nouvelle classe

--- utils/config_management.py
+++ utils/config_management.py
@@ -3,11 +3,18 @@ from pathlib import Path
 
 CONFIG_PATH= Path(__file__).parent.parent / "config.json"
 
+
 def get_config():
+    """
+    Charge la config depuis le fichier json
+    """
     with open(CONFIG_PATH, 'r') as f:
         return json.load(f)
     
 def update_config(key,value):
+    """
+    Change la config en fonction des valeurs entrées en paramètres. Changement notamment des id de message.
+    """
     data = get_config()
     data[key] = value
     with open(CONFIG_PATH, 'w') as f:
