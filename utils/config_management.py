e9d418f70e6dc73b808e049e94db84f16b90fd0e
Tritryi <tristan.fontaniere@outlook.fr>
2026-03-01 01:03:42 +0100

    Ajout d'une méthode de log ainsi que le fichier associé

--- utils/config_management.py
+++ utils/config_management.py
@@ -1,7 +1,9 @@
 import json
 from pathlib import Path
+import datetime
 
 CONFIG_PATH= Path(__file__).parent.parent / "config.json"
+LOG_PATH = Path(__file__).parent.parent / "logs" / "bot.log"
 
 
 def get_config():
@@ -19,3 +21,16 @@ def update_config(key,value):
     data[key] = value
     with open(CONFIG_PATH, 'w') as f:
         json.dump(data, f, indent=4)
+
+
+def logger(message, filename):
+    now = datetime.datetime.now()
+    date = now.strftime("%d-%m-%Y %X")
+
+    with open(LOG_PATH, 'r') as f:
+        line_count = sum(1 for line in f)
+
+    mode = 'w' if line_count >= 50 else 'a'
+    with open (LOG_PATH, mode) as f:
+        log = date + " in " + filename + " : " + str(message)
+        f.write(log+"\n")
